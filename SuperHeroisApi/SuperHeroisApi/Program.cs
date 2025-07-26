ef7a3f572936f3d33fd4117e7f947f63a627a9e8
Wagner Ferreira <wagner-ferreira09@hotmail.com>
2025-07-26 01:45:22 -0300

    Implementação de Middleware para exceções

--- SuperHeroisApi/SuperHeroisApi/Program.cs
+++ SuperHeroisApi/SuperHeroisApi/Program.cs
@@ -1,4 +1,5 @@
 using SuperHeroisApi.CrossCutting;
+using SuperHeroisApi.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +24,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseAuthorization();
 
