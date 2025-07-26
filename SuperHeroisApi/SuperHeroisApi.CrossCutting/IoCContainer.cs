4a573c06924beead8acc5df285518bd9b223a877
Wagner Ferreira <wagner-ferreira09@hotmail.com>
2025-07-25 22:02:02 -0300

    Configuração do EntityFramework e primeira Migration

---
+++ SuperHeroisApi/SuperHeroisApi.CrossCutting/IoCContainer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SuperHeroisApi.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroisApi.CrossCutting
+{
+    public static class IoCContainer
+    {
+        public static IServiceCollection AdicionarServicos(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AdicionarRepositorios(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AdicionarBancoDeDados(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<SuperHeroisApiContext>(options =>
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
---
+++ SuperHeroisApi/SuperHeroisApi.Infra/SuperHeroisApiContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroisApi.Domain.Entidades;
+using SuperHeroisApi.Infra.ConfiguracoesTabelas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroisApi.Infra
+{
+    public class SuperHeroisApiContext : DbContext
+    {
+        public SuperHeroisApiContext(DbContextOptions<SuperHeroisApiContext> options) : base(options) { }
+
+        public DbSet<Herois> Herois { get; set; }
+        public DbSet<Superpoderes> Superpoderes { get; set; }
+        public DbSet<HeroisSuperpoderes> HeroisSuperpoderes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new HeroisConfiguration());
+            modelBuilder.ApplyConfiguration(new SuperpoderesConfiguration());
+            modelBuilder.ApplyConfiguration(new HeroisSuperpoderesConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
